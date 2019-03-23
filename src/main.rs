374fd2fef30344c26cb02cb7da892a9b8a586aaf
Jeremy Soller <jeremy@system76.com>
2019-03-22 22:43:13 -0600

    Update to new area8051

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,69 @@
-use area8051::{Mcu, Rom, Ram};
+use area8051::{Addr, Isa, Mcu, Mem, Reg};
 use std::fs;
+use std::sync::{Mutex, MutexGuard};
 
-use self::iram::IRam;
-mod iram;
-
-use self::xram::XRam;
+use self::xram::xram;
 mod xram;
 
-fn main() {
-    let mut pmem = fs::read("ec.rom").expect("failed to read ec.rom");
-    let mut iram = IRam::new();
-    let mut xram = XRam::new();
+pub struct Ec {
+    mcu: Mutex<Mcu>,
+}
 
-    let mut mcu = Mcu::new(0, pmem, iram, xram);
+impl Ec {
+    pub fn new(pmem: Box<[u8]>) -> Self {
+        Self {
+            mcu: Mutex::new(Mcu::new(pmem))
+        }
+    }
+
+    fn mcu(&self) -> MutexGuard<Mcu> {
+        self.mcu.lock().unwrap()
+    }
+}
+
+impl Mem for Ec {
+    fn load(&self, addr: Addr) -> u8 {
+        match addr {
+            Addr::XRam(i) => {
+                xram(&mut self.mcu(), i, None)
+            },
+            _ => self.mcu().load(addr),
+        }
+    }
+
+    fn store(&mut self, addr: Addr, value: u8) {
+        match addr {
+            Addr::XRam(i) => {
+                xram(&mut self.mcu(), i, Some(value));
+            },
+            _ => self.mcu().store(addr, value),
+        }
+    }
+}
+
+impl Reg for Ec {}
+
+impl Isa for Ec {
+    fn pc(&self) -> u16 {
+        self.mcu().pc()
+    }
+
+    fn set_pc(&mut self, value: u16) {
+        self.mcu().set_pc(value);
+    }
+}
+
+fn main() {
+    let pmem = fs::read("ec.rom").expect("failed to read ec.rom");
+
+    let mut ec = Ec::new(pmem.into_boxed_slice());
+
+    ec.reset();
 
     loop {
-        mcu.step();
+        ec.step();
 
-        if mcu.pc == 0 {
+        if ec.pc() == 0 {
             eprintln!("reset!");
             break;
         }
--- src/xram.rs
+++ src/xram.rs
@@ -1,5 +1,4 @@
-use area8051::{Rom, Ram};
-use std::sync::Mutex;
+use area8051::{Addr, Mcu, Mem};
 
 #[cfg(feature = "debug_xram")]
 macro_rules! debug {
@@ -11,67 +10,36 @@ macro_rules! debug {
     ($($arg:tt)*) => ();
 }
 
-pub struct XRam {
-    data: Mutex<Vec<u8>>
-}
+pub fn xram(mcu: &mut Mcu, address: u16, new_opt: Option<u8>) -> u8 {
+    debug!(" [xram 0x{:02X}", address);
 
-impl XRam {
-    pub fn new() -> XRam {
-        let mut data = vec![0; 65536];
+    let old = mcu.load(Addr::XRam(address));
 
-        // FPCFG
-        data[0x1001] = 0b1011_1111;
-
-        Self {
-            data: Mutex::new(data)
-        }
+    match address {
+        // Scratch SRAM
+        0x0000 ... 0x0FFF => {
+            debug!(" (SRAM)");
+        },
+        // SMFI
+        0x1000 ... 0x107F => {
+            let address = address - 0x1000;
+            debug!(" (SMFI 0x{:02X})", address);
+            match address {
+                0x01 => {
+                },
+                _ => panic!("xram unimplemented SMFI register 0x{:02X}", address)
+            }
+        },
+        _ => panic!("xram unimplemented register 0x{:04X}", address),
     }
 
-    fn transaction(&self, address: u16, new_opt: Option<u8>) -> u8 {
-        let mut data = self.data.lock().unwrap();
-
-        debug!(" [xram 0x{:02X}", address);
-
-        let old = data.load(address);
-
-        match address {
-            // Scratch SRAM
-            0x0000 ... 0x0FFF => {
-                debug!(" (SRAM)");
-            },
-            // SMFI
-            0x1000 ... 0x107F => {
-                let address = address - 0x1000;
-                debug!(" (SMFI 0x{:02X})", address);
-                match address {
-                    0x01 => {
-                    },
-                    _ => panic!("xram unimplemented SMFI register 0x{:02X}", address)
-                }
-            },
-            _ => panic!("xram unimplemented register 0x{:04X}", address),
-        }
-
-        debug!(" load 0x{:02X}", old);
-        if let Some(new) = new_opt {
-            debug!(" store 0x{:02X}", new);
-            data.store(address, new);
-        }
-
-        debug!("]");
-
-        old
+    debug!(" load 0x{:02X}", old);
+    if let Some(new) = new_opt {
+        debug!(" store 0x{:02X}", new);
+        mcu.store(Addr::XRam(address), new);
     }
-}
 
-impl Rom for XRam {
-    fn load(&self, address: u16) -> u8 {
-        self.transaction(address, None)
-    }
-}
+    debug!("]");
 
-impl Ram for XRam {
-    fn store(&mut self, address: u16, value: u8) {
-        self.transaction(address, Some(value));
-    }
+    old
 }
