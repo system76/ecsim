6b3d7317bd7c170330c53e90593fac5cab3bdf1b
Jeremy Soller <jeremy@system76.com>
2019-03-23 08:40:55 -0600

    WIP ec indirect flash access

--- src/xram.rs
+++ src/xram.rs
@@ -23,12 +23,28 @@ pub fn xram(mcu: &mut Mcu, address: u16, new_opt: Option<u8>) -> u8 {
         // SMFI
         0x1000 ... 0x107F => {
             let address = address - 0x1000;
-            debug!(" (SMFI 0x{:02X})", address);
+            debug!(" (SMFI 0x{:02X}", address);
             match address {
-                0x01 => {
+                0x01 => debug!(" FPCFG"),
+                0x3B => debug!(" ECINDAR0"),
+                0x3C => debug!(" ECINDAR1"),
+                0x3D => debug!(" ECINDAR2"),
+                0x3E => debug!(" ECINDAR3"),
+                0x3F => {
+                    debug!(" ECINDDR");
+                    let ecindar = {
+                        (mcu.load(Addr::XRam(0x103B)) as u32) |
+                        (mcu.load(Addr::XRam(0x103C)) as u32) << 8 |
+                        (mcu.load(Addr::XRam(0x103D)) as u32) << 16 |
+                        (mcu.load(Addr::XRam(0x103E)) as u32) << 24
+                    };
+                    debug!(" [flash address 0x{:08X}]", ecindar);
                 },
+                0x58 => debug!(" HINSTC1"),
+                0x63 => debug!(" FLHCTRL3R"),
                 _ => panic!("xram unimplemented SMFI register 0x{:02X}", address)
             }
+            debug!(")");
         },
         _ => panic!("xram unimplemented register 0x{:04X}", address),
     }
